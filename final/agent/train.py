40117bbfe560242fd15f1a3535a0757a8765b80a
javadba <javadba@gmail.com>
2020-12-10 19:16:36 -0800

    Sushil's changes as added by stephen

--- final/agent/train.py
+++ final/agent/train.py
@@ -1,19 +1,15 @@
-from . dense_transforms import Compose, ColorJitter, RandomHorizontalFlip, ToTensor
 from .planner import Planner, save_model
 from .planner import spatial_argmax
 import torch
 import torch.utils.tensorboard as tb
 import numpy as np
-from .utils import load_data2
+from .utils import load_data
 from . import dense_transforms
 import torch.nn.functional as F
-
-# Example command line:
-#   python -m agent.train -p /data/deepteam/quads-1x1-50k -v /data/deepteam/quads-1x1-50k-samples-5k
-#
+import torchvision.transforms as TF
+import sys
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-print(device)
 
 def train(args):
     from os import path
@@ -21,6 +17,7 @@ def train(args):
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
     """
     Your code here, modify your HW4 code
     """
@@ -30,43 +27,40 @@ def train(args):
 
     if args.continue_training:
         model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'detector.th')))
-    loss = torch.nn.BCEWithLogitsLoss(reduction='none')
+    loss = torch.nn.CrossEntropyLoss()
     #loss = torch.nn.BCEWithLogitsLoss()
     #loss = torch.nn.L1Loss()
-    #optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    #optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
 
     import inspect
 
-    # transforms= Compose([ColorJitter(0.2, 0.5, 0.5, 0.2), RandomHorizontalFlip(), ToTensor()])
-    transforms= Compose([RandomHorizontalFlip(), ToTensor()])
-    # transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
-    print(f'Loading csvs from {args.csvpath} and imgs from {args.path} ..')
-    train_data = load_data2(args.path, args.csvpath, transform=transforms, num_workers=args.num_workers)
+    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+
+    #train_data = load_data('3xrandomagentx1250z', transform=transform, num_workers=args.num_workers)
+    #train_data = load_data('emVsAI1kz', transform=transform, num_workers=args.num_workers)
+    #train_data = load_data('2aix2rand2kz', transform=transform, num_workers=args.num_workers)
+    train_data = load_data('2x2x1250z', transform=transform, num_workers=args.num_workers)
+    #train_data = load_data('oneone2500z', transform=transform, num_workers=args.num_workers)
+    #valid_data = load_data('2aix2rand2kz', num_workers=args.num_workers)
     global_step = 0
 
-    batch= 0
-    printBatch = 10
-    avg_loss = 9999.
-    print('Starting training loop..')
     for epoch in range(args.num_epoch):
-        
         model.train()
         losses = []
         acc = []
         for img, label in train_data:
-            if batch % printBatch == 1:
-                print(f'epoch {epoch} batch={batch} avg_loss={avg_loss}')
             img, label = img.to(device), label.to(device)
-            pred = model(img, apply_sigmoid=False)
-            accuracy = ((pred>0).long() == label).detach().cpu().numpy()
+            pred = model(img)
+            accuracy = (pred.argmax(1) == label).detach().cpu().numpy()
+            #accuracy = ((pred>0).long() == label).detach().cpu().numpy()
             acc.extend(accuracy)
             #print(label, pred)
-            loss_val = (loss(pred, label)*pred).mean() / pred.mean()
-            #loss_val = loss(pred, label)
-            if train_logger is not None:
-                train_logger.add_scalar('loss', loss_val, global_step)
-                train_logger.add_scalar('accuracy', np.mean(acc), global_step)
+            #loss_val = (loss(pred, label)*pred).mean() / pred.mean()
+            loss_val = loss(pred, label)
+#            if train_logger is not None:
+#                train_logger.add_scalar('loss', loss_val, global_step)
+#                train_logger.add_scalar('accuracy', np.mean(acc), global_step)
                 #if global_step % 10 == 0:
                     #log(train_logger, img, label, pred, global_step)
 
@@ -76,7 +70,16 @@ def train(args):
             global_step += 1
             losses.append(loss_val.detach().cpu().numpy())
         avg_loss = np.mean(losses)
+#        model.eval()
+#        acc_valid = []
+#        for img, label in valid_data:
+#            img, label = img.to(device), label.to(device)
+#            pred = model(img, apply_sigmoid=False)
+#            accuracy = ((pred>0).long() == label).detach().cpu().numpy()
+#            acc_valid.extend(accuracy)
+#            valid_logger.add_scalar('accuracy', np.mean(acc_valid), global_step)
         print('epoch %-3d \t loss = %0.3f \t acc = %0.3f' % (epoch, avg_loss, np.mean(acc)))
+        sys.stdout.flush()
         save_model(model)
     save_model(model)
 
@@ -99,15 +102,13 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_dir', default='logs')
+    parser.add_argument('--log_dir', default='logs/train2')
     # Put custom arguments here
-    parser.add_argument('-p', '--path')
-    parser.add_argument('-v', '--csvpath')
-    parser.add_argument('-n', '--num_epoch', type=int, default=50)
+    parser.add_argument('-n', '--num_epoch', type=int, default=1)
     parser.add_argument('-w', '--num_workers', type=int, default=4)
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=5e-4)
     parser.add_argument('-c', '--continue_training', action='store_true')
-    # parser.add_argument('-t', '--transform', default='Compose([ToTensor()])')
-    parser.add_argument('-t', '--transform',type=str)
+    parser.add_argument('-t', '--transform', default='Compose([ToTensor()])')
+
     args = parser.parse_args()
     train(args)
--- final/agent/utils.py
+++ final/agent/utils.py
@@ -4,32 +4,25 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms.functional as TF
 from . import dense_transforms
 
+LABEL_NAMES = [0., 1.]
+
 RESCUE_TIMEOUT = 30
 TRACK_OFFSET = 15
-DATASET_PATH = '/content/drive/MyDrive/2x2x1250z'
-CSV_PATH = '/content/drive/MyDrive/2x2x1250z'
+DATASET_PATH = '2x2x1250z'
 
 
 class SuperTuxDataset(Dataset):
-    def __init__(self, dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor(),
-                 csv_path=CSV_PATH, ):
+    def __init__(self, dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor()):
         from PIL import Image
         from glob import glob
         from os import path
         self.data = []
-        k=0
-        cntr = 0
-        LoadCntr = 1000
-        for i,f in enumerate(glob(path.join(csv_path, '*.csv'))):
-            fn = f"{dataset_path}/{f[f.rfind('/')+1:].replace('.csv', '.png')}"
-            if i % LoadCntr == 0:
-                print(f'Loading file[{i}] {fn} ..')
-            i = Image.open(fn)
+        for f in glob(path.join(dataset_path, '*.csv')):
+            i = Image.open(f.replace('.csv', '.png'))
             i.load()
-            self.data.append((i, np.loadtxt(f, dtype=np.float32, delimiter=',')[-1:]))
-            k += 1
-            if k>20000:
-              break
+            label = np.loadtxt(f,delimiter=',')[-1:]
+            label_id = LABEL_NAMES.index(label)
+            self.data.append((i, label_id))
         self.transform = transform
 
     def __len__(self):
@@ -41,10 +34,7 @@ class SuperTuxDataset(Dataset):
         return data
 
 
-def load_data(dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor(), num_workers=0, batch_size=128):
+def load_data(dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor(), num_workers=0, batch_size=64):
     dataset = SuperTuxDataset(dataset_path, transform=transform)
     return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
 
-def load_data2(dataset_path=DATASET_PATH, csv_path=CSV_PATH, transform=dense_transforms.ToTensor(), num_workers=0, batch_size=128):
-    dataset = SuperTuxDataset(dataset_path, transform=transform, csv_path=csv_path)
-    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
